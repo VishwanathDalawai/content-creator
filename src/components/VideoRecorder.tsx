716f08d130294812c05e5946b0d7037be42a2734
vishwanathdalawai <vishwanath.dalawai@arzooo.com>
2024-02-17 21:47:16 +0530

    corrected import file

--- src/components/VideoRecorder.tsx
+++ src/components/VideoRecorder.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState, useEffect } from "react";
-import { getCurrentDate } from "../utils";
+import { getCurrentDate } from "../utils.js";
 
 const VideoRecorder = () => {
   const videoRef = useRef(null);
